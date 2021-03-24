d4f2ae9b8af712afbb0b8eb2f461bf661c7e9b7a
WeightAn <31166841+weightan@users.noreply.github.com>
2021-03-24 17:51:57 +0300

    polygon

--- Hamiltonian_solver.py
+++ Hamiltonian_solver.py
@@ -9,16 +9,69 @@ from scipy.sparse.linalg import eigs
 from scipy.sparse import diags
 from scipy.sparse import dia_matrix
 
-Elevels = 100
+Elevels = 150
 
-images = [20, 30]
+images = [100, 120]
 
-N = 100
+N = 200 
 
 
 inWell = 1
 outWell = 0
 
+
+
+def polygon(sides, radius = 1, rotation = 0, translation = None):
+    one_segment = math.pi * 2 / sides
+
+    points = [
+        (math.sin(one_segment * i + rotation) * radius,
+         math.cos(one_segment * i + rotation) * radius)
+        for i in range(sides)]
+
+    if translation:
+        points = [[sum(pair) for pair in zip(point, translation)]
+                  for point in points]
+
+    return points
+
+
+
+def inPolygon(x, y, xp, yp):
+    
+    c = 0
+    
+    for i in range(len(xp)):
+        if (((yp[i]<=y and y<yp[i-1]) or (yp[i-1]<=y and y<yp[i])) and 
+            (x > (xp[i-1] - xp[i]) * (y - yp[i]) / (yp[i-1] - yp[i]) + xp[i])): c = 1 - c    
+    return c
+
+
+def makePolygonWellMatrix (n, inW, outW):
+    well_matrix = np.empty((n, n))
+
+    sides = 6
+    
+    pol = polygon(sides, 100, 0, (100, 100))
+    
+    xp = tuple([ pol[i][0] for i in range(0, sides) ])
+    yp = tuple([ pol[i][1] for i in range(0, sides) ])
+    
+    print(xp)
+    print(yp)
+    
+    for i in range(0, n):
+        for j in range(0, n):
+            
+            if inPolygon(i, j, xp, yp):
+                
+                well_matrix[i][j] = inW
+                
+            else:
+                
+                well_matrix[i][j] = outW
+                
+    return well_matrix
 
 
 
@@ -100,7 +153,7 @@ def displayAndSaveIm (vectors):
         
         plt.setp(axi, xticks=[], yticks=[])
         
-        plt.savefig( str(i) + 'c100.png', bbox_inches = 'tight') 
+        plt.savefig( str(i) + 'c.png', bbox_inches = 'tight') 
            
         plt.show()
 
@@ -110,7 +163,7 @@ def displayAndSaveIm (vectors):
 if __name__ == '__main__':
 
 
-    mesh = makeCircleWellMatrix(N, inWell, outWell)
+    mesh = makeCircleWellMatrix (N, inWell, outWell)
     
     e_values, e_vec = general_potential(mesh)   
 
@@ -123,7 +176,9 @@ if __name__ == '__main__':
 
     if 1:
         np.save('data_E_vectors_circle' + str(N) +'x'+ str(N) + 'e' + str(Elevels) , e_vec)
-        print('save e_vec done')
+
+
+    print('save e_vec done')
 
     displayAndSaveIm(e_vec)
     
