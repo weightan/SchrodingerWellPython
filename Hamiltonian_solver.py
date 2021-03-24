2dd87c3e408106cf102da09df85ba3548fe40cf5
WeightAn <31166841+weightan@users.noreply.github.com>
2021-03-24 15:44:01 +0300

    added functions

--- Hamiltonian_solver.py
+++ Hamiltonian_solver.py
@@ -1,64 +1,60 @@
 
 
 
-#import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-#from mpl_toolkits.axes_grid1 import make_axes_locatable
-from PIL import Image
 
-from numpy import array, empty
-import numpy.version
-
-from scipy.sparse.linalg import eigs
-from scipy import sparse
-
-
+from scipy.sparse.linalg import eigs 
 from scipy.sparse import diags
 from scipy.sparse import dia_matrix
 
-Elevels = 200
+Elevels = 100
 
-images = [0, 5]
+images = [20, 30]
 
 N = 100
 
 
+inWell = 1
+outWell = 0
 
-def general_potential():
+
+
+
+def makeCircleWellMatrix (n, inW, outW):
+    well_matrix = np.empty((n, n))
+    
+    #00100
+    #01110
+    #00100
+    
+    for i in range(0, n):
+        for j in range(0, n):
+            if math.dist([i, j], [(n-1)/2, (n-1)/2]) <= (n-1)/2 :
+                well_matrix[i][j] = inW
+            else:
+                well_matrix[i][j] = outW
+                
+    return well_matrix
+
+
+
+
+
+def general_potential(matrixWell2D):
     
     ################################################################################
      
-    
 
-    
-    binary_well_matrix = empty((N, N), None)    
-    
-    
-    for i in range(0, N):
-        for j in range(0, N):
-            if math.dist([i, j], [(N-1)/2, (N-1)/2]) <= (N-1)/2:
-                binary_well_matrix[i][j] = 1
-            else:
-                binary_well_matrix[i][j] = 0
-    
-               
-    #global position_mesh
-    position_mesh = binary_well_matrix
-    #print(position_mesh)
-    position_mesh = np.matrix.flatten(position_mesh)
+    position_mesh = np.matrix.flatten( matrixWell2D )
 
-    #for debag
-    #position_mesh = [1 for i in position_mesh ]
-    #
-    
-    ################################################################################
+
 
     No_points = N*N
     x_intervals = np.linspace(0, 1, N)
     increment = np.absolute(x_intervals[0] - x_intervals[1])
-    print (increment)
+    print ('increment ' + str(increment))
     
     ################################################################################
     
@@ -70,9 +66,9 @@ def general_potential():
     zeroV = 4 / increment
     
     diagmN =  [incrementValue * position_mesh[i] for i in range(0, No_points - N )]
-    diagm1 =  [incrementValue * position_mesh[i] for i in range(0, No_points )]
-    diag0  =  [zeroV for i in range(0, No_points+1)]
-    diagp1 =  [incrementValue * position_mesh[i] for i in range(0, No_points)]
+    diagm1 =  [incrementValue * position_mesh[i] for i in range(0, No_points     )]
+    diag0  =  [              zeroV               for i in range(0, No_points+1   )]
+    diagp1 =  [incrementValue * position_mesh[i] for i in range(0, No_points     )]
     diagpN =  [incrementValue * position_mesh[i] for i in range(0, No_points - N )]
 
     diagK = [-N, -1, 0, 1, N]
@@ -95,29 +91,12 @@ def general_potential():
 
 
 
- 
-e_values, e_vec = general_potential()   
 
-
-idx = e_values.argsort()[::-1]   
-e_values = e_values[idx]
-e_vec = e_vec[:,idx]
-
-print('vectors done')
-
-if 1:
-    np.save('data_E_vectors_circle_100x100', e_vec)
-
-
-print('save e_vec done')
-
-
-def displayAndSaveIm ():
+def displayAndSaveIm (vectors):
     for i in range(int(images[0]), int(images[1])):
         figi, axi = plt.subplots(1, 1)
            
-        plot = plt.imshow( pow( np.absolute( e_vec[:,i].reshape(N,N) ) ,2), cmap='nipy_spectral', interpolation='gaussian') 
-        
+        plot = plt.imshow( pow( np.absolute( vectors[:,i].reshape(N,N) ) ,2), cmap='nipy_spectral', interpolation='gaussian') 
         
         plt.setp(axi, xticks=[], yticks=[])
         
@@ -125,10 +104,32 @@ def displayAndSaveIm ():
            
         plt.show()
 
-displayAndSaveIm()
 
-       
-print('****************************** all done *******************************')
+
+
+if __name__ == '__main__':
+
+
+    mesh = makeCircleWellMatrix(N, inWell, outWell)
+    
+    e_values, e_vec = general_potential(mesh)   
+
+
+    idx = e_values.argsort()[::-1]   
+    e_values = e_values[idx]
+    e_vec = e_vec[:,idx]
+
+    print('vectors done')
+
+    if 1:
+        np.save('data_E_vectors_circle' + str(N) +'x'+ str(N) + 'e' + str(Elevels) , e_vec)
+
+
+    print('save e_vec done')
+
+    displayAndSaveIm(e_vec)
+    
+    print('****************************** all done *******************************')
 
 
 
