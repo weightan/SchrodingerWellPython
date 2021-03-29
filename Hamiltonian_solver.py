ebea54c85d2db51dfc9f1ec5c5f8818e43dc3352
WeightAn <31166841+weightan@users.noreply.github.com>
2021-03-29 22:00:11 +0300

    Update Hamiltonian_solver.py

--- Hamiltonian_solver.py
+++ Hamiltonian_solver.py
@@ -1,26 +1,30 @@
 
 
-
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 
 from scipy.sparse.linalg import eigs 
-from scipy.sparse import diags
-from scipy.sparse import dia_matrix
+from scipy.sparse import diags, dia_matrix
 
-Elevels = 150
+#calculate this number of eigenvectors = number of energy levels
+Elevels = 120
 
-images = [100, 120]
+#image this eigenvectors
+images = [80, 100]
 
+# size of matrix N x N for well
+# set N = 30 to reduce the calculation time. 
+# You shouldn't set more than 500 otherwise the computation time will increase to hours
 N = 200 
 
-
 inWell = 1
+
+#set 0.1 or so to reduce the calculation time. 
 outWell = 0
 
 
-
+#for polygon well matrix
 def polygon(sides, radius = 1, rotation = 0, translation = None):
     one_segment = math.pi * 2 / sides
 
@@ -36,7 +40,7 @@ def polygon(sides, radius = 1, rotation = 0, translation = None):
     return points
 
 
-
+#for polygon well matrix
 def inPolygon(x, y, xp, yp):
     
     c = 0
@@ -46,7 +50,7 @@ def inPolygon(x, y, xp, yp):
             (x > (xp[i-1] - xp[i]) * (y - yp[i]) / (yp[i-1] - yp[i]) + xp[i])): c = 1 - c    
     return c
 
-
+#for polygon well matrix
 def makePolygonWellMatrix (n, inW, outW):
     well_matrix = np.empty((n, n))
 
@@ -74,7 +78,7 @@ def makePolygonWellMatrix (n, inW, outW):
     return well_matrix
 
 
-
+#to make the matrix in the form of a circle
 def makeCircleWellMatrix (n, inW, outW):
     well_matrix = np.empty((n, n))
     
@@ -88,7 +92,6 @@ def makeCircleWellMatrix (n, inW, outW):
                 well_matrix[i][j] = inW
             else:
                 well_matrix[i][j] = outW
-                
     return well_matrix
 
 
@@ -96,37 +99,29 @@ def makeCircleWellMatrix (n, inW, outW):
 
 
 def general_potential(matrixWell2D):
-    
-    ################################################################################
-     
 
     position_mesh = np.matrix.flatten( matrixWell2D )
 
-
-
     No_points = N*N
+
     x_intervals = np.linspace(0, 1, N)
-    increment = np.absolute(x_intervals[0] - x_intervals[1])
-    print ('increment ' + str(increment))
-    
-    ################################################################################
-    
-    
-    
-    
-    increment = pow(increment, 2)
+
+    increment = pow(x_intervals[1], 2)
+
     incrementValue = -1/increment
+
     zeroV = 4 / increment
     
     diagmN =  [incrementValue * position_mesh[i] for i in range(0, No_points - N )]
-    diagm1 =  [incrementValue * position_mesh[i] for i in range(0, No_points     )]
-    diag0  =  [              zeroV               for i in range(0, No_points+1   )]
-    diagp1 =  [incrementValue * position_mesh[i] for i in range(0, No_points     )]
+    diagm1 =  [incrementValue * position_mesh[i] for i in range(0, No_points - 1 )]
+    diag0  =  [              zeroV               for i in range(0, No_points     )]
+    diagp1 =  [incrementValue * position_mesh[i] for i in range(0, No_points - 1 )]
     diagpN =  [incrementValue * position_mesh[i] for i in range(0, No_points - N )]
 
-    diagK = [-N, -1, 0, 1, N]
+    diagsK = [-N, -1, 0, 1, N]
+    diagsV = [diagmN, diagm1, diag0, diagp1, diagpN]
 
-    Hamiltonian = diags([diagmN, diagm1, diag0, diagp1, diagpN],  diagK, format = 'dia')
+    Hamiltonian = diags(diagsV, diagsK, format = 'dia')
     
     print('Hamiltonian values done')
 
@@ -136,20 +131,20 @@ def general_potential(matrixWell2D):
     #Hamiltonian.tocsr()
     e_values, e_vec = eigs(Hamiltonian, k = Elevels )
 
-
     print('All Hamiltonian done')
+
     ################################################################################
-    
+
     return [e_values, e_vec]
 
 
 
 
-def displayAndSaveIm (vectors):
+def displayAndSaveIm (vectorsToImage):
     for i in range(int(images[0]), int(images[1])):
         figi, axi = plt.subplots(1, 1)
            
-        plot = plt.imshow( pow( np.absolute( vectors[:,i].reshape(N,N) ) ,2), cmap='nipy_spectral', interpolation='gaussian') 
+        plot = plt.imshow( pow( np.absolute( vectorsToImage[:,i].reshape(N,N) ) ,2), cmap='nipy_spectral', interpolation='gaussian') 
         
         plt.setp(axi, xticks=[], yticks=[])
         
@@ -165,6 +160,8 @@ if __name__ == '__main__':
 
     mesh = makeCircleWellMatrix (N, inWell, outWell)
     
+    #mesh = makePolygonWellMatrix (N, inWell, outWell)
+    
     e_values, e_vec = general_potential(mesh)   
 
 
@@ -172,23 +169,15 @@ if __name__ == '__main__':
     e_values = e_values[idx]
     e_vec = e_vec[:,idx]
 
+    #array for picture of energy level i:  Elevel = pow(np.absolute( e_vec[:,i].reshape(N,N) ), 2)
+    #pictures are e_vec, not e_values
+
     print('vectors done')
 
     if 1:
         np.save('data_E_vectors_circle' + str(N) +'x'+ str(N) + 'e' + str(Elevels) , e_vec)
-
-
-    print('save e_vec done')
+        print ('save e_vec done')
 
     displayAndSaveIm(e_vec)
     
     print('****************************** all done *******************************')
-
-
-
-    
-
-
-
-  
-
